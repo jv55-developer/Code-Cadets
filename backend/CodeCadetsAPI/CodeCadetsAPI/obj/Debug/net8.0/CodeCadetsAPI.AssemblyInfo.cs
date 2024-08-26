301c9d61ecd1697237eef2918c73c722d5a9ee93
Trevor Damoyi <trevordamoyi307@gmail.com>
2024-08-26 23:47:23 +0200

    Done with the JWT Authentication

--- backend/CodeCadetsAPI/CodeCadetsAPI/obj/Debug/net8.0/CodeCadetsAPI.AssemblyInfo.cs
+++ backend/CodeCadetsAPI/CodeCadetsAPI/obj/Debug/net8.0/CodeCadetsAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeCadetsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b334b1a85e9be078b5b205647c30e8d373c330d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97bc46214e1660b9efdc123de9ea80f2bed623a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeCadetsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeCadetsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
