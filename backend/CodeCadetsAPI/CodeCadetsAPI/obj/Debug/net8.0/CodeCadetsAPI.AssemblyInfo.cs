13d6e203eaba712c5c0ce3270560be34ebc68ba9
Trevor Damoyi <trevordamoyi307@gmail.com>
2024-08-30 16:38:22 +0200

    Updates

--- backend/CodeCadetsAPI/CodeCadetsAPI/obj/Debug/net8.0/CodeCadetsAPI.AssemblyInfo.cs
+++ backend/CodeCadetsAPI/CodeCadetsAPI/obj/Debug/net8.0/CodeCadetsAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeCadetsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97bc46214e1660b9efdc123de9ea80f2bed623a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa9550cfd38822dfb34ecbf2220ea10da3ccf67a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeCadetsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeCadetsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
